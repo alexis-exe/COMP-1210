044dee13958677f6c1e3c068032ef323084a79ea
Alexis Frison <55997298+alexis-exe@users.noreply.github.com>
2019-11-04 18:41:48 -0600

    Add files via upload

---
+++ Activities/Activity_04/UserInfoDriver.java
@@ -0,0 +1,27 @@
+/**.
+*
+* Alexis Frison-1210-003
+* 9/16/19
+*/
+public class UserInfoDriver
+{
+   /**
+   * @param args Command-line not used
+   */
+   public static void main(String[] args)
+   {
+      UserInfo user1 = new UserInfo("Pat", "Doe");
+      System.out.println("\n" + user1);
+      user1.setLocation("Auburn");
+      user1.setAge(19);
+      user1.logOn();
+      System.out.println("\n" + user1);
+      
+      UserInfo user2 = new UserInfo("Sam", "Jones");
+      System.out.println("\n" + user2);
+      user2.setLocation("Atlanta");
+      user2.setAge(21);
+      user2.logOn();
+      System.out.println("\n" + user2);
+   }
+}
