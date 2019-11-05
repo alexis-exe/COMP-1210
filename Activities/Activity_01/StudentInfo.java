d58e451b2b3a3ecc963bff04b1addb4a6fa51f67
Alexis Frison <55997298+alexis-exe@users.noreply.github.com>
2019-11-04 18:21:03 -0600

    Add files via upload

---
+++ Activities/Activity_01/StudentInfo.java
@@ -0,0 +1,21 @@
+/** 
+* Prints Student Info.
+*
+* Activity 01
+* Alexis Frison-1210-003
+* 8/26/19
+*/
+public class StudentInfo
+{
+/**
+* Prints name and previous courses.
+* @param args Command line arguments - not used
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Alexis Frison");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+   }
+   
+}
