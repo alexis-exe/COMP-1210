cb8dbc90ebfcf19695f69d7f261f79fce8ac3394
Alexis Frison <55997298+alexis-exe@users.noreply.github.com>
2019-12-06 21:23:04 -0600

    Create DivisionDriver.java

---
+++ Activities/Activity_11/DivisionDriver.java
@@ -0,0 +1,42 @@
+ import javax.swing.JOptionPane;
+	  	
+ /** Alexis Frison-1210-003.  	
+   * 11/18/19*/
+  	
+ public class DivisionDriver  	
+ {  	
+    /** @param args command line arguments. */
+  	
+    public static void main(String[] args)  	
+    {  	
+       String numInput = JOptionPane.showInputDialog("Enter the numerator:"); 	
+       String denomInput = JOptionPane.showInputDialog("Enter the denominator:");
+       
+       try  	
+       {  	
+          int num = Integer.parseInt(numInput);  	
+          int denom = Integer.parseInt(denomInput);
+  	
+          String result = "Integer division: \n" 	
+                         + Division.intDivide(num, denom)  	
+                         + "\n\nFloating point division: \n"  	
+                         + Division.decimalDivide(num, denom);
+                         
+          JOptionPane.showMessageDialog(null, result,   	
+                "Result", JOptionPane.PLAIN_MESSAGE);  	
+       }
+  	
+       catch (NumberFormatException e)  	
+       {  	
+          JOptionPane.showMessageDialog(null,  	
+                   "Invalid input: enter numerical integer values only.",  	
+                   "Error", JOptionPane.ERROR_MESSAGE); 	
+       }
+       
+       catch (IllegalArgumentException e)	  	
+       {  	
+          JOptionPane.showMessageDialog(null,   	
+               e, "Error", JOptionPane.ERROR_MESSAGE);  	
+       }	  	
+    }	  	
+ }
